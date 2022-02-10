129325eec9b7f6ccac805295a63a06d527a825e1
delta <nishchal98765@gmail.com>
2022-02-10 23:54:31 +0530

    hey

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-
 from telegram import *
 from telegram.ext import *
-
+import os
 api_key = "5170347007:AAHRLYqkz0qvOdS0NzwE5JDdgdF1mRr55RE"
-bot = Bot(api_key)
-# print(bot.get_me())        --> to get values of our bot (or to check)
-updater = Updater(api_key, use_context=True)
-disp = updater.dispatcher
 
 
 def check1(update, context):
@@ -14,17 +9,25 @@ def check1(update, context):
 
 
 def handleplz(update, context):
-    update.message.reply_text(
-        "is it working ")
+    update.message.reply_text("is it working ")
 
 
 def error(update, context):
     update.message.reply_text("ooops! there is an error")
 
 
-disp.add_handler(CommandHandler("verma", check1))
-disp.add_handler(MessageHandler(Filters.text, handleplz))
-disp.add_error_handler(error)
+def main():
+    updater = Updater(api_key, use_context=True)
+    disp = updater.dispatcher
 
-updater.start_polling()
-updater.idle()
+    disp.add_handler(CommandHandler("verma", check1))
+    disp.add_handler(MessageHandler(Filters.text, handleplz))
+    disp.add_error_handler(error)
+
+    updater.start_webhook(listen="0.0.0.0", port=os.environ.get(
+        "PORT", 443), url_path=api_key, webhook_url="https://deltaxpr.herokuapp.com/"+api_key)
+    updater.idle()
+
+
+if __name__ == "__main__":
+    main()
