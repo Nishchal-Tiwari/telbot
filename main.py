864255d8eed0b36d8c866d3a6719557ffbdd357d
delta <nishchal98765@gmail.com>
2022-02-12 21:41:45 +0530

    thsi is a new update

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ def check1(update, context):
 
 
 def handleplz(update, context):
-    update.message.reply_text(f"is it working {update.message.text}")
+    if(update.message.text == "abhishek")
+    {
+        update.message.reply_text("is a vhutiya londa")
+    }
 
 
 def error(update, context):
