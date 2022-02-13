e0297896a0accbea5a962faeed028e57a9f03d97
delta <nishchal98765@gmail.com>
2022-02-13 18:33:56 +0530

    this is a sample message

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ def check1(update, context):
 
 
 def handleplz(update, context):
-    if(update.message.text == "abhishek"):
-        update.message.reply_text("is a vhutiya londa")
+    if(update.message.text == "Nishchal"):
+        update.message.reply_text("is a good boy")
 
 
 def error(update, context):
